adaec9b07b8517339f39f49facc58c6da2a83135
msaswata15 <121812665+msaswata15@users.noreply.github.com>
2025-07-25 09:28:25 +0530

    Exercise2 of React All files uploaded

---
+++ Week6_React/Exercise2_ReactJS_HOL/code/studentapp/src/App.js
@@ -0,0 +1,16 @@
+
+import Home from './Conponents/Home';
+import About from './Conponents/About';
+import Contact from './Conponents/Contact';
+function App() {
+  return (
+    <div className="Container" style={{ justifyContent: 'center', textAlign: 'center', padding: '20px' }}>
+      <Home />
+      <About />
+      <Contact />
+
+    </div >
+  );
+}
+
+export default App;
