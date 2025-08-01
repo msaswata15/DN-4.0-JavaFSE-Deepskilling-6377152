322216028923170f8e598ff647de3e6d0771adfa
msaswata15 <121812665+msaswata15@users.noreply.github.com>
2025-08-01 11:18:53 +0530

    Exercise 12 of React HandsOn Uploaded

---
+++ Week7_React/4_HandsOnIn12ReactJSHOL/code/ticketbookingapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import LoginControl from './components/LoginControl';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginControl />
+      </header>
+    </div>
+  );
+}
+
+export default App;
